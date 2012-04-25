7a4de033f0fa0d7c8f7ef0a0d5f17620baac2e3b
Sowmya <thangasowmya@payoda.com>
2012-04-25 16:23:47 +0530

    Required files added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestSample::Application.config.secret_token = 'c2e8608d566bf5412b006a56f7a0f4f8130c81efcf752602d0aa26577b5c34fdf05237669993b99371fd9559db8ed21f0919e69dfe0be4a244fc9efbe25c9e3b'
